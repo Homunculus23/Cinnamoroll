0955fc130884ea37a08880d24a409a07639f3573
fpatr <fpatr@qq.com>
2022-09-01 20:20:18 +0800

    第三次修改拉，不管了啊啊啊！

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 const _audioSwitch = $('.audioSwitch');
 const _audioStyle =$('.audioStyle')
 
+let onOff = 0;
+
 _audioSwitch.on('click',() => {
-    _audioStyle[0].play();
+    onOff++;
+    if(onOff%2 === 1){
+        _audioStyle[0].play();
+        _audioSwitch.css('box-shadow','0 0 15px blue' )
+    }else if(onOff%2 === 0){
+        _audioStyle[0].pause();
+        _audioSwitch.css('box-shadow','0 0 15px red' )
+    }
 })
